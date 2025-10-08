e89244092fe0b5acd5877c1aec84638b20052075
Piotr Matejek <devmatejekpior@gmail.com>
2025-10-08 13:31:18 +0200

    solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -8,13 +8,41 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail('test@mail.com')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
-      .toBeTruthy();
+  it(`should return true for a valid email`, () => {
+    expect(validateEmail('test@mail.com')).toBe(true);
+    expect(validateEmail('user.name-12@mail.co.uk')).toBe(true);
+    expect(validateEmail('a@b.c')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return false when email does not contain @`, () => {
+    expect(validateEmail('testmail.com')).toBe(false);
+  });
+
+  it(`should return false when personal part starts with dot`, () => {
+    expect(validateEmail('.test@mail.com')).toBe(false);
+  });
+
+  it(`should return false when personal part ends with dot`, () => {
+    expect(validateEmail('test.@mail.com')).toBe(false);
+  });
+
+  it(`should return false when there are double dots in personal part`, () => {
+    expect(validateEmail('te..st@mail.com')).toBe(false);
+  });
+
+  it(`should return false when domain starts with dot`, () => {
+    expect(validateEmail('test@.mail.com')).toBe(false);
+  });
+
+  it(`should return false when forbidden characters are used`, () => {
+    expect(validateEmail('test!@mail.com')).toBe(false);
+    expect(validateEmail('user&name@mail.com')).toBe(false);
+  });
+
+  it(`should return false for missing domain extension`, () => {
+    expect(validateEmail('test@mail')).toBe(false);
+  });
 });
